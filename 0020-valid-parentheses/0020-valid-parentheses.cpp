c8696a1236731ac2bab2a5bf38b3566c5acde741
Aryan Singh <143319448+AryanSing833@users.noreply.github.com>
2026-06-25 14:43:44 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.03%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char it : s) {
+
+            if (it == '(' || it == '{' || it == '[') {
+                st.push(it);
+            }
+            else {
+
+                if (st.empty())
+                    return false;
+
+                char ch = st.top();
+                st.pop();
+
+                if (it == ')' && ch != '(')
+                    return false;
+
+                if (it == '}' && ch != '{')
+                    return false;
+
+                if (it == ']' && ch != '[')
+                    return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
