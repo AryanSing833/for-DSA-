d823ff752c180317220b07720c4196ec2ea4cc43
Aryan Singh <143319448+AryanSing833@users.noreply.github.com>
2026-07-08 15:07:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (51.76%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int solve(vector<int>& cost, vector<int>& dp) {
         dp[0] = cost[0];
         dp[1] = cost[1];
-        for (int i = 2; i < cost.size(); i++) {
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        for (int i=2; i<cost.size(); i++) {
+            dp[i] = cost[i]+min(dp[i-1], dp[i-2]);
         }
         return min(dp[cost.size()-1], dp[cost.size()-2]);
     }
